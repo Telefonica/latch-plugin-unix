7fe993d04f3401d26b4f3f2c417cd4ef2841533f
Ivan Martin <ivan.martin@11paths.com>
2014-11-19 08:41:08 +0100

    fix test and add README

--- test/integration_test/latchtest.py
+++ test/integration_test/latchtest.py
@@ -21,11 +21,16 @@
 
 import unittest
 import random
+import re
 import shutil
 import os
 
 from subprocess import *
 
+
+
+LATCH_CONFIG_DEFAULT = "/etc/latch/latch.conf"
+LATCH_ACC_DEFAULT = "/etc/latch/latch.accounts"
 
 
 def internal_test(f):
@@ -36,7 +41,7 @@ def internal_test(f):
     return f
 
 
-def modify_conf(key, value, cfile="/etc/latch/latch.conf"):
+def modify_conf(key, value, cfile=LATCH_CONFIG_DEFAULT):
     if not os.path.isfile(cfile + ".orig"):
         shutil.copy2(cfile, cfile + ".orig")  
 
@@ -54,7 +59,7 @@ def modify_conf(key, value, cfile="/etc/latch/latch.conf"):
 
     shutil.copystat(cfile + ".orig", cfile)
 
-def modify_acc(key, value, afile="/etc/latch/latch.accounts"):
+def modify_acc(key, value, afile=LATCH_ACC_DEFAULT):
     if not os.path.isfile(afile + ".orig"):
         shutil.copy2(afile, afile + ".orig")  
 
@@ -74,208 +79,208 @@ def restore_file(f):
     shutil.copy2(f + ".orig", f) 
     os.remove(f + ".orig")
 
-def getConfigParameter(name, configFile="/etc/latch/latch.conf"):
-    try:
+def get_config(key, configFile=LATCH_CONFIG_DEFAULT):
+    if os.path.isfile(configFile):
         f = open(configFile,"r")
-    except IOError as e:
-        return None
+        lines = f.readlines()
+        f.close()
 
-    lines = f.readlines()
-    f.close()
+        for line in lines:
+            regex = r'\s*' + re.escape(key) + r'\s*=\s*(?P<value>.+)'
+            res = re.match(regex, line)
 
-    for line in lines:
-        if line.find(name) != -1:
-            break;
+            if res != None:
+                return res.group('value') 
 
-    words = line.split()
-    if len(words) == 3:
-        return words[2]
     return None
 
 
 class TestLatch(unittest.TestCase):
 
-    def setUp(self):
-        self.user = "latch_test_user"
-        self.password = str(random.random())
-        call(["./adduser.sh", self.user, self.password], stdout=PIPE, stderr=PIPE)
+    @classmethod
+    def setUpClass(cls):
+        TestLatch.user = "latch_test_user"
+        TestLatch.password = str(random.random())
+        call(["./adduser.sh", TestLatch.user, TestLatch.password], stdout=PIPE, stderr=PIPE)
 
-    def tearDown(self):
-        call(["sudo", "su", self.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
-        call(["sudo", "deluser", self.user], stdout=PIPE, stderr=PIPE)
+    @classmethod
+    def tearDownClass(cls):
+        call(["sudo", "su", TestLatch.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
+        call(["sudo", "deluser", TestLatch.user], stdout=PIPE, stderr=PIPE)
 
 
     def test_pair_with_invalid_configuration(self):
         modify_conf("app_id", "XXXX")
 
-        response = Popen(["sudo", "su", self.user, "-c", "latch -p XXXXXX"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -p XXXXXX"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
 
-        restore_file("/etc/latch/latch.conf")
+        restore_file(LATCH_CONFIG_DEFAULT)
         self.assertEqual("Invalid Application ID or Secret Key\n", err)
 
     def test_pair_invalid_token(self):
-        response = Popen(["sudo", "su", self.user, "-c", "latch -p XXXXXX"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -p XXXXXX"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
         self.assertEqual("Invalid token\n", err)
 
     def test_pair_invalid_token_len(self):
-        response = Popen(["sudo", "su", self.user, "-c", "latch -p XXXXXXXX"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -p XXXXXXXX"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
         self.assertEqual("Invalid token\n", err)
 
     def test_pair_invalid_token_chars(self):
-        response = Popen(["sudo", "su", self.user, "-c", "latch -p *10OI_"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -p *10OI_"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
         self.assertEqual("Invalid token\n", err)
 
     def test_pair_blank_token(self):
-        response = Popen(["sudo", "su", self.user, "-c", "latch -p"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -p"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
         self.assertTrue("Option -p requires an argument." in err)
 
     def test_status_being_unpaired(self):
-        call(["sudo", "su", self.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
+        call(["sudo", "su", TestLatch.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
 
-        response = Popen(["sudo", "su", self.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
-        self.assertEqual("The user " + self.user + " is not paired with latch\n", err)
+        self.assertEqual("The user " + TestLatch.user + " is not paired with latch\n", err)
 
     def test_status_without_acc_file(self):
-        shutil.copy2("/etc/latch/latch.accounts", "/etc/latch/latch.accounts.orig") 
-        os.remove("/etc/latch/latch.accounts")
+        shutil.copy2(LATCH_ACC_DEFAULT, LATCH_ACC_DEFAULT + ".orig") 
+        os.remove(LATCH_ACC_DEFAULT)
 
-        response = Popen(["sudo", "su", self.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
 
-        restore_file("/etc/latch/latch.accounts")
+        restore_file(LATCH_ACC_DEFAULT)
 
-        self.assertEqual("The user " + self.user + " is not paired with latch\n", err)
+        self.assertEqual("The user " + TestLatch.user + " is not paired with latch\n", err)
 
     def test_status_invalid_acc_id(self):
-        modify_acc(self.user, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        modify_acc(TestLatch.user, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
-        response = Popen(["sudo", "su", self.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
 
-        restore_file("/etc/latch/latch.accounts")
+        restore_file(LATCH_ACC_DEFAULT)
 
-        self.assertEqual("The user " + self.user + " is not really paired with latch. Try to unpair and pair again.\n", err)
+        self.assertEqual("The user " + TestLatch.user + " is not really paired with latch. Try to unpair and pair again.\n", err)
 
     def test_op_status_being_unpaired(self):
-        call(["sudo", "su", self.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
+        call(["sudo", "su", TestLatch.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
 
-        response = Popen(["sudo", "su", self.user, "-c", "latch -o test_op"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -o test_op"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
-        self.assertEqual("The user " + self.user + " is not paired with latch\n", err)
+        self.assertEqual("The user " + TestLatch.user + " is not paired with latch\n", err)
 
     def test_op_status_without_acc_file(self):
-        shutil.copy2("/etc/latch/latch.accounts", "/etc/latch/latch.accounts.orig") 
-        os.remove("/etc/latch/latch.accounts")
+        shutil.copy2(LATCH_ACC_DEFAULT, LATCH_ACC_DEFAULT + ".orig") 
+        os.remove(LATCH_ACC_DEFAULT)
 
-        response = Popen(["sudo", "su", self.user, "-c", "latch -o test_op"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -o test_op"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
 
-        restore_file("/etc/latch/latch.accounts")
+        restore_file(LATCH_ACC_DEFAULT)
 
-        self.assertEqual("The user " + self.user + " is not paired with latch\n", err)
+        self.assertEqual("The user " + TestLatch.user + " is not paired with latch\n", err)
 
     def test_op_status_invalid_operation(self):
-        response = Popen(["sudo", "su", self.user, "-c", "latch -o invalid_op"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -o invalid_op"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
 
-        self.assertEqual("Operation invalid_op does not exist in /etc/latch/latch.conf\n", err)
+        self.assertEqual("Operation invalid_op does not exist in " + LATCH_CONFIG_DEFAULT + "\n", err)
 
     def test_op_status_invalid_acc_id(self):
-        modify_acc(self.user, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        modify_acc(TestLatch.user, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
-        response = Popen(["sudo", "su", self.user, "-c", "latch -o test_op"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -o test_op"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
 
-        restore_file("/etc/latch/latch.accounts")
+        restore_file(LATCH_ACC_DEFAULT)
 
-        self.assertEqual("The user " + self.user + " is not really paired with latch. Try to unpair and pair again.\n", err)
+        self.assertEqual("The user " + TestLatch.user + " is not really paired with latch. Try to unpair and pair again.\n", err)
 
     def test_unpair_being_unpaired(self):
-        call(["sudo", "su", self.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
+        call(["sudo", "su", TestLatch.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
 
-        response = Popen(["sudo", "su", self.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
-        self.assertEqual("The user " + self.user + " is not paired with latch\n", err)
+        self.assertEqual("The user " + TestLatch.user + " is not paired with latch\n", err)
 
     def test_unpair_without_acc_file(self):
-        shutil.copy2("/etc/latch/latch.accounts", "/etc/latch/latch.accounts.orig") 
-        os.remove("/etc/latch/latch.accounts")
+        shutil.copy2(LATCH_ACC_DEFAULT, LATCH_ACC_DEFAULT + ".orig") 
+        os.remove(LATCH_ACC_DEFAULT)
 
-        response = Popen(["sudo", "su", self.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
 
-        restore_file("/etc/latch/latch.accounts")
+        restore_file(LATCH_ACC_DEFAULT)
 
-        self.assertEqual("The user " + self.user + " is not paired with latch\n", err)
+        self.assertEqual("The user " + TestLatch.user + " is not paired with latch\n", err)
 
     @internal_test
     def test_all_correct(self):
         import latchclient
 
         app = latchclient.LatchClient() 
+        app.authenticate("THE_LATCH_ACCOUNT", "PASSWORD")
+
         res = app.pairing_token()
         token = res.get_data()["token"]
 
         # pair
-        response = Popen(["sudo", "su", self.user, "-c", "latch -p " + token], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -p " + token], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
-        self.assertEqual("Account successfully paired to the user " + self.user + "\n", out)
+        self.assertEqual("Account successfully paired to the user " + TestLatch.user + "\n", out)
 
         # try pair again
-        response = Popen(["sudo", "su", self.user, "-c", "latch -p " + token], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -p " + token], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
-        self.assertEqual("The user " + self.user + " is already paired with latch\n", err)
+        self.assertEqual("The user " + TestLatch.user + " is already paired with latch\n", err)
 
-        app_id = getConfigParameter("app_id")
-        app = latchclient.LatchClient()
+        app_id = get_config("app_id")
         app.status_update(app_id, "on")
 
         # status locked
-        response = Popen(["sudo", "su", self.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
         self.assertTrue("Status: latch locked\n", err)
 
         app.status_update(app_id, "off")
 
         # status unlocked
-        response = Popen(["sudo", "su", self.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
         self.assertTrue("Status: latch not locked\n", err)
 
-        op_id = getConfigParameter("test_op")
+        op_id = get_config("test_op")
         app.status_update(op_id, "on")
 
         # operation status locked
-        response = Popen(["sudo", "su", self.user, "-c", "latch -o test_op"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -o test_op"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
         self.assertTrue("Status: latch locked\n", err)
 
         app.status_update(op_id, "off")
 
         # operation status unlocked
-        response = Popen(["sudo", "su", self.user, "-c", "latch -o test_op"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -o test_op"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
         self.assertTrue("Status: latch not locked\n", err)
 
         # status invalid configuration
         modify_conf("app_id", "XXXX")
 
-        response = Popen(["sudo", "su", self.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -s"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
 
-        restore_file("/etc/latch/latch.conf")
+        restore_file(LATCH_CONFIG_DEFAULT)
         self.assertEqual("Invalid Application ID or Secret Key\n", err)
 
         # unpair
-        response = Popen(["sudo", "su", self.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
+        response = Popen(["sudo", "su", TestLatch.user, "-c", "latch -u"], stdout=PIPE, stderr=PIPE)
         out, err = response.communicate()
-        self.assertEqual("Account belonging to the user " + self.user + " successfully unpaired from latch\n", out)
+        self.assertEqual("Account belonging to the user " + TestLatch.user + " successfully unpaired from latch\n", out)
 
 
 
